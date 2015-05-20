c3d70b8ca4953d910d520af67aab2cb7ae24909b
MarkUfland <mark.ufland@sky.com>
2015-05-20 18:39:25 +0100

    Added Database Project

---
+++ SOLID2/dbo/Tables/FXData.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[FXData] (
+    [Id]     INT             IDENTITY (1, 1) NOT NULL,
+    [FXDate] DATETIME        NULL,
+    [FXRate] DECIMAL (18, 5) NULL,
+    CONSTRAINT [PK_FXData] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
