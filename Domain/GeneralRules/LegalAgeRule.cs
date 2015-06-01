b2c4ba4ce08a3c64637f4efa3048a6738ab3b661
MarkUfland <mark.ufland@sky.com>
2015-06-01 16:59:23 +0100

    Legal Age Rule Added

---
+++ Domain/GeneralRules/LegalAgeRule.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.GeneralRules
+{
+    public abstract class LegalAgeRule : IRule
+    {
+        private int Age;
+
+        public LegalAgeRule(int age)
+        {
+            Age = age;
+        }
+
+        public RuleResult ExecuteRule(ServiceCommand serviceCommand)
+        {
+            var ruleResult = new RuleResult() { HasPassed = serviceCommand.Age >= Age };
+
+            return ruleResult;
+        }
+    }
+}
