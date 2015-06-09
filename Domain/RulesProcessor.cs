45ec17f6c623be434a9cd22835b1f26413dbbf63
MarkUfland <mark.ufland@sky.com>
2015-06-09 16:38:18 +0100

    Added Rules Processor to repository

---
+++ Domain/RulesProcessor.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class RulesProcessor : IRulesProcessor
+    {
+        private IRulesBuilder rulesBuilder;
+        private IList<IRule> rules;
+
+        public RulesProcessor()
+        {
+        }
+
+        public void RulesProcessorSetup(IRulesBuilder rulesBuilder)
+        {
+            this.rulesBuilder = rulesBuilder;
+            this.rulesBuilder.PrioritiseRules();
+        }
+
+        public void RunRules(ServiceCommand serviceCommand)
+        {
+            foreach (IRule rule in rules)
+            {
+                RuleResult ruleResult = rule.ExecuteRule(serviceCommand);
+            }
+        }
+    }
+
+}
